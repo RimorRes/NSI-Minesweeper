e0959f7cc740615f7a2ca65c3e08747534e06e48
Maxime Djmb <>
2021-04-27 13:07:40 +0000

    added mine generation

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
-from random import randint
+import random
 
 def mines():
   minelist = []
   counter = 0 
 
   while len(minelist) < 10:
-    x = randint(0,7)
-    y = randint(0,12)
+    x = random.randint(0,7)
+    y = random.randint(0,12)
     pos = [x,y]
     minelist.append(pos)
     counter += 1 
