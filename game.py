312fbab800585e4ebcb64cad18da2660b0848ed8
Ian McFarland <>
2021-04-27 12:59:47 +0000

    added mine generation

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
-print("Hellow World")
+from random import randint
+
+def mines():
+  minelist = []
+  counter = 0 
+
+  while len(minelist) < 10:
+    x = randint(0,7)
+    y = randint(0,12)
+    pos = [x,y]
+    minelist.append(pos)
+    counter += 1 
+    print(minelist)
+
+    for i in range(len(minelist)-1):
+      if pos == minelist[i]:
+        minelist.remove[i]
+
+mines()
