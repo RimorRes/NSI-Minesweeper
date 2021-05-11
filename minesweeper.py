0c1157a9cc9a09c252da8658a77942a76454d204
Oxyde2 <maxime.djomby@gmail.com>
2021-05-11 19:51:53 +0200

    Added splash screen

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,7 @@
 import random
 import tkinter as tk
 from PIL import ImageTk, Image
+from time import sleep
 
 
 def generate_mines(width, height, number_mines, excluded):
@@ -26,45 +27,75 @@ def delimit_start_area(start_x, start_y, width, height, radius=1):
                 area.append([x, y])
     return area
 
-#gui = tk.Tk()
-#diff = tk.Label(gui, text = "Difficulty")
-#diff.pack()
-#easy = tk.Button(gui, text = "Easy", command = gui.destroy)
-#easy.pack()
-#medium = tk.Button(gui, text = "Medium", command = gui.destroy)
-#medium.pack()
-#hard = tk.Button(gui, text = "Hard", command = gui.destroy)
-#hard.pack()
-#gui.mainloop
+# gui = tk.Tk()
+# diff = tk.Label(gui, text = "Difficulty")
+# diff.pack()
+# easy = tk.Button(gui, text = "Easy", command = gui.destroy)
+# easy.pack()
+# medium = tk.Button(gui, text = "Medium", command = gui.destroy)
+# medium.pack()
+# hard = tk.Button(gui, text = "Hard", command = gui.destroy)
+# hard.pack()
+# gui.mainloop
+
+
+class SplashScreen(tk.Toplevel):
+
+    def __init__(self, master):
+        super().__init__(master)
+        self.title("Splash Screen Example")
+        self.overrideredirect(True)
+
+        screen_width = master.winfo_screenwidth()
+        screen_height = master.winfo_screenheight()
+        x = (screen_width/2) - (1280/2)
+        y = (screen_height/2) - (640/2)
+        self.geometry(f'1280x640+{int(x)}+{int(y)}')
+
+        self.canvas = tk.Canvas(self, width=1280, height=640, bd=-2)
+        self.canvas.pack()
+        self.img = ImageTk.PhotoImage(
+                Image.open('naval_mine6.png')
+                .resize((1280, 640))
+            )
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
+
+        self.update()
 
 
 class Game(tk.Frame):
 
-    def __init__(self, master, width, height, geometry_width, geometry_height, tile_size, number_mines):
-        super().__init__()
+    def __init__(self, master, width, height, tile_size, number_mines):
+        # Setting up the frame
+        super().__init__(master)
         self.master = master
-        self.master.title("Minesweeper")
         self.pack(side=tk.BOTTOM)
 
-        self.gwidth, self.gheight = geometry_width, geometry_height # Geometry of the canvas
-
-        self.canvas = tk.Canvas(self, width= self.gwidth - 76, height= self.gheight - 76) #Dimensions of actual game
-        self.canvas.bind('<Button-1>', self.handle_left_click) #Binding clicks to have functions
-        self.canvas.bind('<Button-3>', self.handle_right_click)
-        self.canvas.pack() 
-
-        self.move_counter = 0
-
-        self.width, self.height = width, height
+        # Game/tile dimensions
+        self.width, self.height = width, height  # columns and rows
         self.tile_size = tile_size
 
-        # Matrix which contains the number of adjacent mines
-        self.tile_states = [[0] * self.width for _ in range(self.height)]
+        # Setting up canvas
+        self.canvas = tk.Canvas(  # Dimensions of actual game + remove thin border
+            self,
+            width=self.width*self.tile_size,
+            height=self.height*self.tile_size,
+            bd=-2)
+        self.canvas.bind('<Button-1>', self.handle_left_click)  # Binding clicks to have functions
+        self.canvas.bind('<Button-3>', self.handle_right_click)
+        self.canvas.pack()
+
+        # Move counter
+        self.move_counter = 0
+
+        # Setting up tile states
+        self.tile_states = [[0] * self.width for _ in range(self.height)]  # matrix for number of adjacent mines
         self.number_mines = number_mines
         self.mine_positions = []
         self.flag_positions = []
         self.discovered_tiles = []
 
+        # Handling game icon/images
         self.tile_images = {}
         tiles = ['tile', 'flag', 'bomb', 'hidden', 'wrong', '0', '1', '2', '3', '4', '5', '6', '7', '8']
         for t in tiles:
@@ -73,6 +104,7 @@ class Game(tk.Frame):
                 .resize((self.tile_size, self.tile_size))
             )
 
+        # Setting up the starting grid
         self.init_grid()
 
     def handle_left_click(self, event):
@@ -144,74 +176,95 @@ class Game(tk.Frame):
                 self.place_tile(x, y, self.tile_images['tile'])
 
     def place_tile(self, x, y, img):
-        self.canvas.create_image(x * self.tile_size + 2, y * self.tile_size + 2, anchor=tk.NW, image=img)
+        self.canvas.create_image(x * self.tile_size, y * self.tile_size, anchor=tk.NW, image=img)
 
-def GameEasy(): # Easy difficulty
-  gui.destroy()
-  root = tk.Tk()
-  root.title("MINESWEEPER")
-  #root.configure(background = "gray")
-  root.geometry("480x720")
-  game = Game(root, 8, 13, 480, 720, 50, 10)
 
-def GameNormal(): # Normal difficulty
-  gui.destroy()
-  root = tk.Tk()
-  root.title("MINESWEEPER")
-  #root.configure(background = "gray")
-  root.geometry("580x820")
-  game = Game(root, 13, 15, 580, 820, 50, 40)
+def game_easy():  # Easy difficulty
+    gui.destroy()
+    root = tk.Tk()
+    root.title("Minesweeper")
+    # root.configure(background="gray")
+    root.geometry("480x720")
+    Game(
+        master=root,
+        width=8,
+        height=13,
+        tile_size=50,
+        number_mines=10
+    )
 
-def GameHard(): # Hard difficulty
-  gui.destroy()
-  root = tk.Tk()
-  root.title("MINESWEEPER")
-  #root.configure(background = "gray")
-  root.geometry("1130x640")
-  game = Game(root, 30, 16, 1130, 640, 35, 99)
 
-def GameChoose(event): # Choose your own difficulty
-  gui.destroy()
-  root = tk.Tk()
-  root.title("MINESWEEPER")
-  #root.configure(background = "gray")
-  game = Game(root)
+def game_normal():  # Normal difficulty
+    gui.destroy()
+    root = tk.Tk()
+    root.title("Minesweeper")
+    # root.configure(background="gray")
+    root.geometry("580x820")
+    Game(
+        master=root,
+        width=13,
+        height=15,
+        tile_size=50,
+        number_mines=40
+    )
 
+
+def game_hard():  # Hard difficulty
+    gui.destroy()
+    root = tk.Tk()
+    root.title("Minesweeper")
+    # root.configure(background = "gray")
+    root.geometry("1130x640")
+    Game(
+        master=root,
+        width=30,
+        height=16,
+        tile_size=35,
+        number_mines=99
+    )
+
+# TODO: Choose your own difficulty
+# def game_choose(event):  # Choose your own difficulty
+#     gui.destroy()
+#     root = tk.Tk()
+#     root.title("Minesweeper")
+#     root.configure(background="gray")
+#     Game(root)
 
 
 if __name__ == "__main__":
-    gui = tk.Tk() #Creating GUI
-    gui.title("gui")
-    gui.configure(background = "#d2e3d0")
+
+    gui = tk.Tk()  # Creating GUI
+
+    # Splash screen
+    splash = SplashScreen(gui)
+    gui.withdraw()
+    sleep(2)
+    splash.destroy()
+    gui.deiconify()
+
+    gui.title("Menu")
+    gui.configure(background="#d2e3d0")
     gui.geometry("300x500")
-    diff = tk.Label(gui, text = "Difficulty")
+    diff = tk.Label(gui, text="Difficulty")
     diff.pack(pady=5)
-    easy = tk.Button(gui, text = "Easy", command = GameEasy) #Easy mode button
+    easy = tk.Button(gui, text="Easy", command=game_easy)  # Easy mode button
     easy.pack(pady=5)
-    medium = tk.Button(gui, text = "Normal", command = GameNormal) #Normal mode button
+    medium = tk.Button(gui, text="Normal", command=game_normal)  # Normal mode button
     medium.pack(pady=5)
-    hard = tk.Button(gui, text = "Hard", command = GameHard) #Hard mode button
+    hard = tk.Button(gui, text="Hard", command=game_hard)  # Hard mode button
     hard.pack(pady=5)
 
+    canvas = tk.Canvas(width=100, height=100)  # Canvas for choose your own
 
-
-
-    canvas = tk.Canvas(width = 100, height = 100) # Canvas for choose your own
-    
-    def number(): 
-      pass
-
-    number = tk.Label(canvas, text = "Enter a Number")
+    number = tk.Label(canvas, text="Enter a Number")
     number.pack(pady=20)
     box = tk.Entry(canvas)
-    box.pack(pady = 10)
-    button = tk.Button(canvas, text = "Confirm number", command = number)
+    box.pack(pady=10)
+    button = tk.Button(canvas, text="Confirm number")
     button.pack(pady=20)
-    canvaslabel = tk.Label(gui)
-    
+    canvas_label = tk.Label(gui)
+
     canvas.pack()
 
-
     gui.mainloop()
-
-
